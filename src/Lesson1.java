84d2ea134c720e1153a055fb8c535c1432e4e846
Karina.Likhosherstova <k.umyarova@gmail.com>
2015-09-25 20:04:11 +0300

    Create project from book

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -2,7 +2,15 @@
  * Created by IT_School on 20.09.2015.
  */
 public class Lesson1 {
-    public static void main(String [] args){
-        System.out.println("Hello World! и Привет!");
+    /*public static void main(String [] args){
+        System.out.println("Hello World! Привет  всем!");
+    }*/
+    public static void main (String args[]) {
+        int num;
+        num = 100;
+        System.out.println("Этo переменная num: " + num);
+        num = num * 2;
+        System.out.println("Знaчeниe переменной num * 2 равно ");
+        System.out .println(num);
     }
-}
+    }
